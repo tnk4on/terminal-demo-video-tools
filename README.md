3b01df2a9622f5cb6135c1cac4e414f9afdd6670
tnk4on <tnk4on@gmail.com>
2023-04-28 17:12:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Container images for terminal-demo-video
+
+## Build
+```
+git clone https://github.com/tnk4on/terminal-demo-video-tools.git
+cd terminal-demo-video-tools
+./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -eu
+
+# Repository Login
+echo "### login to docker.io ###"
+podman login docker.io
+
+echo -e "\n### login to quay.io ###"
+podman login quay.io
+
+#export TAG=ffmpeg
+export DOCKER=docker.io/tnk4on
+export QUAY=quay.io/tnk4on
+export ENVFILE=env
+
+# Build all container images
+CURDIR=$PWD
+cd Containerfile.d
+
+for f in Containerfile*
+do
+    echo -e "\n### Build multi-arch images for ${f/Containerfile./} ###"
+    for ARCH in amd64 arm64
+    do
+        echo -e "\n#### podman build -t ${f/Containerfile./}:${ARCH} -f $f --build-arg ARCH=${ARCH} --platform linux/${ARCH} --manifest ${f/Containerfile./} . ####"
+        podman build -t ${f/Containerfile./}:${ARCH} -f $f --build-arg ARCH=${ARCH} --platform linux/${ARCH} --manifest ${f/Containerfile./} .
+    done
+done
+
+# Test
+
+for f in Containerfile*
+do
+    echo -e "\n### Run ${f/Containerfile./} ###"
+    for ARCH in amd64 arm64
+    do
+        echo -e "\n#### podman run --rm ${f/Containerfile./}:${ARCH} --version ####"
+        podman run --rm ${f/Containerfile./}:${ARCH} --version
+    done
+done
+
+
+# Push
+## Docker.io
+echo -e "\n### push to docker.io ###"
+for f in Containerfile*
+do
+    echo -e "\n#### podman manifest push --all ${f/Containerfile./}:latest docker://${DOCKER}/${f/Containerfile./}:latest --format docker ###"
+    podman manifest push --all ${f/Containerfile./}:latest docker://${DOCKER}/${f/Containerfile./}:latest --format docker
+done
+
+### Quay.io
+echo -e "\n### push to quay.io ###"
+for f in Containerfile*
+do
+    echo -e "\n#### podman manifest push --all ${f/Containerfile./}:latest docker://${QUAY}/${f/Containerfile./}:latest ####"
+    podman manifest push --all ${f/Containerfile./}:latest docker://${QUAY}/${f/Containerfile./}:latest
+done
